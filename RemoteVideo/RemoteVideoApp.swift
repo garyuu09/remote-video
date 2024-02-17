85517f2c2493b9ee7f559a256a22954d1689cacf
garyuu09 <ribereo.minami@gmail.com>
2024-02-17 17:17:17 +0900

    Initial Commit

---
+++ RemoteVideo/RemoteVideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteVideoApp.swift
+//  RemoteVideo
+//
+//  Created by Ryuga on 2024/02/17.
+//
+
+import SwiftUI
+
+@main
+struct RemoteVideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
